712729f35fc4d6a1750a5c3830cfde4dd87c1b35
Verônica Ribeiro <veronicaribeiroofficial@gmail.com>
2024-12-07 18:34:42 -0300

    Modifica o código para usar o token

--- src/main/java/com/dailycommitsbot/DailyCommitsBot.java
+++ src/main/java/com/dailycommitsbot/DailyCommitsBot.java
@@ -2,6 +2,17 @@ package com.dailycommitsbot;
 
 public class DailyCommitsBot {
     public static void main(String[] args) {
+
+        // Lê o token do GitHub a partir da variável de ambiente
+        String githubToken = System.getenv("GITHUB_TOKEN");
+
+        // Verifica se o token foi encontrado
+        if (githubToken == null) {
+            System.out.println("Token de GitHub não encontrado!");
+            return;
+        }
+        
+        // Imprime no console o sucesso na obtenção do token
         System.out.print("Bot de commits diários funcionando!");
     }
 }
