7d4c4058502f03e032234bff0b7534a48fcce98a
Verônica Ribeiro <veronicaribeiroofficial@gmail.com>
2024-12-07 17:12:56 -0300

    Testando mensagem inicial

---
+++ src/main/java/com/dailycommitsbot/DailyCommitsBot.java
@@ -0,0 +1,7 @@
+package com.dailycommitsbot;
+
+public class DailyCommitsBot {
+    public static void main(String[] args) {
+        System.out.print("Bot de commits diários funcionando!");
+    }
+}
